dad53f4d84e7dd88ebf15c2430a674524492a68e
marek1n <marek1nemecek@gmail.com>
2022-10-05 18:22:04 +0200

    initial

---
+++ src/stimuli.js
@@ -0,0 +1,34 @@
+var wordList = [
+    {
+      "word": "radio",
+      "category": "hShE"
+    },
+    {
+      "word": "handshake",
+      "category": "hShE"
+    },
+    {
+      "word": "knockout",
+      "category": "hSlE"
+    },
+    {
+      "word": "carriage",
+      "category": "hSlE"
+    },
+    {
+      "word": "teal",
+      "category": "lShE"
+    },
+    {
+      "word": "mile",
+      "category": "lShE"
+    },
+    {
+      "word": "microbe",
+      "category": "lSlE"
+    },
+    {
+      "word": "vaccine",
+      "category": "lSlE"
+    }
+  ]
